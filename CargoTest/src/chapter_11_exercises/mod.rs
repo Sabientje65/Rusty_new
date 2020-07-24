5f5d51d2eeb1949f480245d93d3700dc7c267caf
Danny Bogers <d.bogers@leadq.nl>
2020-07-24 13:44:15 +0200

    Continue with chapter 11

--- CargoTest/src/chapter_11_exercises/mod.rs
+++ CargoTest/src/chapter_11_exercises/mod.rs
@@ -27,13 +27,60 @@ impl exercise::exc::Exercise for Chapter11 {
     }
 }
 
+struct Rect {
+    w: u32,
+    h: u32
+}
+
+impl Rect {
+    fn new(w: u32, h: u32) -> Rect {
+        Rect { w, h }
+    }
+    
+    fn can_hold(&self, other: &Rect) -> bool{
+        return self.w > other.w && self.h > other.h;
+    }
+}
+
+fn add_two(a: u32) -> u32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works(){
         assert_eq!(5 + 5, 10)
     }
 
+    #[test]
+    fn larger_can_hold_smaller(){
+        let larger = Rect::new(10, 5);
+        let smaller = Rect::new(9, 4);
+        
+        assert!(larger.can_hold(&smaller))
+    }
+    
+    #[test]
+    fn smaller_cannot_hold_larger(){
+        let larger = Rect::new(10, 5);
+        let smaller = Rect::new(9, 4);
+
+        assert!(!smaller.can_hold(&larger))
+    }
+    
+    #[test]
+    fn it_adds_two(){
+        assert_eq!(add_two(2), 4)
+    }
+    
+    #[test]
+    fn it_does_not_add_one(){
+        assert_ne!(add_two(2), 3)
+    }
+    
     // #[test]
     // fn should_not_panic(){
     //     panic!()
