647801fdd57657b511f57859ff6730ddbf755a47
Danny Bogers <dannybogers4@gmail.com>
2020-08-03 00:35:07 +0200

    Continue with chapter 12

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,12 +1,49 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(&config.file)?;
+    let contents = fs::read_to_string(&config.file)?;
 
-    println!("With text:\n{}", content);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    
+    for matched_line in results {
+        println!("{}", matched_line);
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+           matches.push(line) 
+        }
+    }
+
+    matches
+}
+
+fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 impl Config {
@@ -18,9 +55,12 @@ impl Config {
             return Err("Minimum of 2 arguments required");
         }
 
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        
         let config = Config {
             query: args[Config::POS_QUERY].clone(),
-            file: args[Config::POS_FILE].clone()
+            file: args[Config::POS_FILE].clone(),
+            case_sensitive
         };
         
         Ok(config)
@@ -29,6 +69,40 @@ impl Config {
 
 pub struct Config {
     pub query: String,
-    pub file: String
+    pub file: String,
+    pub case_sensitive: bool
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
 
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,15 +10,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!(" Problem parsing arguments: {}", err);
         process::exit(error_codes::INSUFFICIENT_ARGS);
     });
-    
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file);
 
     if let Err(e) = minigrep::run(&config) {
-        println!("Application error: {}", e);
         process::exit(error_codes::APP_ERROR);
     }
 }
