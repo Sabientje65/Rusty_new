52d90c4e81b80a03dbed21fd4603e98d577a9d43
Danny Bogers <dannybogers4@gmail.com>
2020-08-04 23:15:55 +0200

    Wrap up chapter 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -10,10 +10,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(error_codes::INSUFFICIENT_ARGS);
     });
 
     if let Err(e) = minigrep::run(&config) {
+        eprintln!("Application error: {}", e);
+        
         process::exit(error_codes::APP_ERROR);
     }
 }
