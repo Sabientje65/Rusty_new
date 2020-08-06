5a075f56af4fa5d5ba755500744b47adf4a27af4
Danny Bogers <dannybogers4@gmail.com>
2020-08-06 23:52:24 +0200

    Hashmap based lazy, we /tired/

--- CargoTest/src/chapter_13_exercises/mod.rs
+++ CargoTest/src/chapter_13_exercises/mod.rs
@@ -1,36 +1,40 @@
 use crate::exercise;
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
+use std::ops::Deref;
 
 struct LazyNum<T: Fn(u32) -> u32> {
     calculation: T,
-    value: Option<u32>
+    value_map: HashMap<u32, u32>
 }
 
 impl<T: Fn(u32) -> u32> LazyNum<T> {
     fn new(calculation: T) -> LazyNum<T> {
         LazyNum {
             calculation,
-            value: None
+            value_map: HashMap::new()
         }
     }
     
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
+        let calculation = &self.calculation;
+        
+        *self.value_map.entry(arg)
+            .or_insert_with(|| { 
+                println!("Executing fn");
+                (calculation)(arg) 
+            })
+        
+        // *self.value_map.entry(arg)
+        //     .or_insert_with(|| { return (self.calculation)(arg); })
     }
 }
 
 pub fn run(){
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
-
+    
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
@@ -41,15 +45,36 @@ fn generate_workout(intensity: u32, random_number: u32) {
         num
     });
     
-    
-    if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result.value(intensity));
-        println!("Next, do {} situps!", expensive_result.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
+    let mut do_thing = |new_intensity: u32| {
+        // println!("Today, do {} pushups!", expensive_result.value(new_intensity));
+        // println!("Next, do {} situps!", expensive_result.value(new_intensity));
+        
+        if new_intensity < 25 {
+            println!("Today, do {} pushups!", expensive_result.value(new_intensity));
+            println!("Next, do {} situps!", expensive_result.value(new_intensity));
         } else {
-            println!("Today, run for {} minutes!", expensive_result.value(intensity));
+            if random_number == 3 {
+                println!("Take a break today! Remember to stay hydrated!");
+            } else {
+                println!("Today, run for {} minutes!", expensive_result.value(new_intensity));
+            }
         }
-    }
+    };
+
+    do_thing(intensity);
+    do_thing(intensity + 1);
+    do_thing(intensity);
+    // (&do_thing)(intensity + 1);
+    // (&do_thing)(intensity);
+    
+    // if intensity < 25 {
+    //     println!("Today, do {} pushups!", expensive_result.value(intensity));
+    //     println!("Next, do {} situps!", expensive_result.value(intensity));
+    // } else {
+    //     if random_number == 3 {
+    //         println!("Take a break today! Remember to stay hydrated!");
+    //     } else {
+    //         println!("Today, run for {} minutes!", expensive_result.value(intensity));
+    //     }
+    // }
 }
